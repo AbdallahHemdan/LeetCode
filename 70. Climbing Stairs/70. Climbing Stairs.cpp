bcea420cd7afe2d3bf31609166d49645f0ec5bd9
Abdallah Hemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-07-03 15:04:27 +0200

    Update 70. Climbing Stairs.cpp

--- 70. Climbing Stairs/70. Climbing Stairs.cpp
+++ 70. Climbing Stairs/70. Climbing Stairs.cpp
@@ -1,3 +1,4 @@
+// DP Solution
 class Solution {
 public:
     int climbStairs(int n) {
@@ -8,5 +9,25 @@ public:
         for(int i=2 ;i<=n ;i++)
             dp[i] = dp[i-1] + dp[i-2];
         return dp[n];
+    }
+};
+
+
+// Recursion with Memoization Algorithm
+class Solution {
+public:
+    int climbStairs(int n) {
+        int mem[n+1];
+        memset(mem,0,sizeof(mem));
+        return Climb_Stairs(0,n,mem);
+    }
+    
+    int Climb_Stairs(int i, int n, int mem[]){
+        if(i > n) return 0;
+        if(i == n) return 1;
+        
+        if(mem[i] > 0) return mem[i];
+        mem[i] = Climb_Stairs(i+1,n,mem) + Climb_Stairs(i+2,n,mem);
+        return mem[i];
     }
 };
